5c0504ca221b6e5abad10c9e2897e376ef994e48
Renan da Silva Oliveira Andrade <renan.silva3@pucpr.edu.br>
2024-08-23 12:24:01 -0300

    arquivos

---
+++ 23082024/1.js
@@ -0,0 +1,8 @@
+
+function somar() {
+    var valor1 = parseInt(document.getElementById("valor1").value); 
+    var valor2 = parseInt(document.getElementById("valor2").value);
+
+    var soma = valor1 + valor2;
+    alert(soma)
+}
